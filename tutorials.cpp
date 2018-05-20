07ffae6e1449a070abaebc6590fb2ae26879fd42
AlexAlvrzCastell <39219220+AlexAlvrzCastell@users.noreply.github.com>
2018-05-20 10:16:18 -0500

    Actualización de la clase
    Se corrige el toString de la clase
    los string no aceptan enteros como tal, toma su valor ascii y lo añade a la cadena, se agrega la función sprintf para dicha acción

--- tutorials.cpp
+++ tutorials.cpp
@@ -1,59 +1,50 @@
 #include "tutorials.h"
 ///Constructor Base
-Tutorials::Tutorials()
-{
+Tutorials::Tutorials() {
     hours = 0;
-}
+    }
 
 ///Constructor Copia
-Tutorials::Tutorials(const Tutorials& e) : name(e.name), hours(e.hours){ }
+Tutorials::Tutorials(const Tutorials& e) : name(e.name), hours(e.hours) { }
 
 ///Constructor Parametrizado
 Tutorials::Tutorials(const Name& n, Date& i, Date& f, int& h) : name(n), initialDate(i), finishDate(f), hours(h) { }
 
-Name Tutorials::getName()
-{
+Name Tutorials::getName() {
     return name;
-}
+    }
 
-Date Tutorials::getInitialDate()
-{
+Date Tutorials::getInitialDate() {
     return initialDate;
-}
+    }
 
-Date Tutorials::getFinishDate()
-{
+Date Tutorials::getFinishDate() {
     return finishDate;
-}
+    }
 
-int Tutorials::getHours()
-{
+int Tutorials::getHours() {
     return hours;
-}
+    }
 
-void Tutorials::setName(const Name& n)
-{
+void Tutorials::setName(const Name& n) {
     name = n;
-}
+    }
 
-void Tutorials::setInitialDate(const Date& i)
-{
+void Tutorials::setInitialDate(const Date& i) {
     initialDate = i;
-}
+    }
 
-void Tutorials::setFinishDate(const Date& f)
-{
+void Tutorials::setFinishDate(const Date& f) {
     finishDate = f;
-}
+    }
 
-void Tutorials::setHours(const int& h)
-{
+void Tutorials::setHours(const int& h) {
     hours = h;
-}
+    }
 
-std::string Tutorials::toString()
-{
+std::string Tutorials::toString() {
     std::string result;
+    char x[5];
 
     result = "Nombre: ";
     result += name.toString();
@@ -62,8 +53,8 @@ std::string Tutorials::toString()
     result += "Fecha de Termino: ";
     result += finishDate.toString();
     result += "Cantidad de Horas Semanales: ";
-    result += hours;
+    sprintf(x, "%02i", hours);
+    result += x;
 
     return result;
-}
-
+    }
